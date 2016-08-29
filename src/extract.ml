53a49871d59d5b81730e9ec7e64c58f7b8345bc8
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2016-08-29 07:53:35 +0530

    Added core

--- src/extract.ml
+++ src/extract.ml
@@ -1,7 +1,11 @@
+open Core.Std
+open Tar_gzip.Reader
 
-let extract file_name =
+let extract ?dest file_name  =
     let gzip_chan = Gzip.open_in file_name in
-    let l = Tar_gzip.Reader.Archive.list ~level:Tar_gzip.Reader.Header.Posix gzip_chan in
-    List.iter (fun h -> print_string Tar_gzip.Reader.Header.(h.file_name ^ " " ^  (Int64.to_string h.file_size) ^ " " ^ (Link.to_string h.link_indicator)); print_newline ()) l
+
+    let l = Archive.list ~level:Header.Posix gzip_chan in
+
+    List.iter ~f:(fun h -> print_string Header.(h.file_name ^ " " ^  (Int64.to_string h.file_size) ^ " " ^ (Link.to_string h.link_indicator)); print_newline ()) l
 
 let () = extract "NetworkInput.unitypackage"
