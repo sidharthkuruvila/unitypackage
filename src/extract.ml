4f02e21f75f6c52a7ff91f64a1720dabfcb0a85e
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2016-08-30 09:51:51 +0530

    Added extract to unzip a tar.gz file

--- src/extract.ml
+++ src/extract.ml
@@ -1,11 +1,29 @@
 open Core.Std
 open Tar_gzip.Reader
 
-let extract ?dest file_name  =
-    let gzip_chan = Gzip.open_in file_name in
 
+
+
+let list_files file_name  =
+    let gzip_chan = Gzip.open_in file_name in
     let l = Archive.list ~level:Header.Posix gzip_chan in
 
     List.iter ~f:(fun h -> print_string Header.(h.file_name ^ " " ^  (Int64.to_string h.file_size) ^ " " ^ (Link.to_string h.link_indicator)); print_newline ()) l
+    
 
-let () = extract "NetworkInput.unitypackage"
+exception UnsupportedLinkType
+
+let extract ?desto file_name = 
+    let open Header in
+    let dest = Option.value ~default:"." desto in
+    let read_header h =
+        match h.link_indicator with
+            Link.Normal -> h.file_name |> Filename.concat dest |> Pervasives.open_out |> Option.some
+          | Link.Directory -> Unix.mkdir (Filename.concat dest h.file_name); None 
+          | _ -> raise UnsupportedLinkType in
+    let gzip_chan = Gzip.open_in file_name in
+    Archive.extract_gen read_header gzip_chan
+
+let () = 
+    Tar.Header.compatibility_level := Tar.Header.Posix;
+    extract ~desto:"dump" "NetworkInput.unitypackage"
--- src/tar_gzip.ml
+++ src/tar_gzip.ml
@@ -1,11 +1,13 @@
+open Core.Std
+
 module Read_Gzip_Write_File = struct
   type in_channel = Gzip.in_channel
-  type out_channel = Pervasives.out_channel
+  type out_channel = Pervasives.out_channel option
 
   let really_input = Gzip.really_input
   let input = Gzip.input
-  let output = Pervasives.output
-  let close_out = Pervasives.close_out
+  let output c = c |> Option.map ~f:Pervasives.output |> Option.value ~default:(fun s i j -> ())
+  let close_out = Option.iter ~f:Pervasives.close_out
 end
 
 
